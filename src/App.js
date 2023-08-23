12b4f5b20e7c85ad07cc7b1b1802986fadc1a6e5
Sofia <sofia.cronstrom@gmail.com>
2023-08-23 16:26:08 +0200

    chapter 21

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App (){
+
+
+    return(
+    <input style={{border: "3px solid green"}} placeholder="Hi there"/>        
+    )
+    };
+
+    export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-function App (){
-
-
-return(
-<input type="number" min={5} style={{border: "3px solid green"}}/>        
-)
-};
 
 root.render(<App/>)
